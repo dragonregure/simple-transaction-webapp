e6c09d312b80d202c33b3a438d42c7fe6be3b002
dragonregure <dragonregure@gmail.com>
2026-06-20 19:30:09 +0700

    Improve sidebar

--- backend-laravel/resources/views/layouts/partials/sidebar.blade.php
+++ backend-laravel/resources/views/layouts/partials/sidebar.blade.php
@@ -9,8 +9,8 @@
     </div>
 
     <div class="sidebar-wrapper">
-        <nav class="mt-2" aria-label="Main navigation">
-            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" data-accordion="false">
+        <nav class="mt-2 h-100" aria-label="Main navigation">
+            <ul class="nav sidebar-menu flex-column h-100" data-lte-toggle="treeview" data-accordion="false">
                 <x-admin.nav-item
                     route="transactions.index"
                     icon="bi bi-receipt"
@@ -21,16 +21,76 @@
                     icon="bi bi-file-earmark-spreadsheet"
                     label="Reports"
                 />
-                <x-admin.nav-item
-                    route="chart-of-accounts.index"
-                    icon="bi bi-journal-text"
-                    label="Master Chart of Accounts"
-                />
-                <x-admin.nav-item
-                    route="chart-of-account-categories.index"
-                    icon="bi bi-tags"
-                    label="Master Chart of Account Categories"
-                />
+                <li class="nav-item has-treeview @if (request()->routeIs('chart-of-accounts.*', 'chart-of-account-categories.*')) menu-open @endif">
+                    <a href="#" @class([
+                        'nav-link',
+                        'active' => request()->routeIs('chart-of-accounts.*', 'chart-of-account-categories.*'),
+                    ])>
+                        <i class="nav-icon bi bi-database"></i>
+                        <p>
+                            Master Data
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <x-admin.nav-item
+                            route="chart-of-account-categories.index"
+                            icon="bi bi-tags"
+                            label="Chart of Account Categories"
+                        />
+                        <x-admin.nav-item
+                            route="chart-of-accounts.index"
+                            icon="bi bi-journal-text"
+                            label="Chart of Accounts"
+                        />
+                    </ul>
+                </li>
+                <li class="nav-item has-treeview menu-open mt-auto">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-link-45deg"></i>
+                        <p>
+                            Other Links
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a
+                                href="https://github.com/dragonregure/simple-transaction-webapp"
+                                class="nav-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <i class="nav-icon bi bi-github"></i>
+                                <p>Repository</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="https://getlifely.vercel.app/"
+                                class="nav-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <span class="nav-icon sidebar-lifely-icon" aria-hidden="true">
+                                    <img src="/images/lifely-icon.png" alt="">
+                                </span>
+                                <p>Get Lifely</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a
+                                href="https://github.com/dragonregure/lifely"
+                                class="nav-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <i class="nav-icon bi bi-github"></i>
+                                <p>Lifely Repository</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </nav>
     </div>
